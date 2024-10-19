b2485146e4ad8118095989f37eeab9bd6cffd73e
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-10-19 12:09:10 +0530

    added support for login to huggingface hub

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import os
 import torch
+import huggingface_hub
+
+from dotenv import load_dotenv
+
 from transformers import HfArgumentParser
 from src.train import train
 from src.config import ScriptArguments
 
+load_dotenv()
 torch.manual_seed(42)
 
 if __name__ == "__main__":
     __parser = HfArgumentParser(ScriptArguments)
     script_args: ScriptArguments = __parser.parse_args_into_dataclasses()[0]
+
+    huggingface_hub.login(os.getenv("HF_ACCESS_TOKEN"))
+
     match (script_args.mode):
         case "train":
             train(script_args)
