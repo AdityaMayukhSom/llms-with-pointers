10712e6138a61b3a6139fdb38a0d450d927182e3
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-10-21 23:28:55 +0530

    added test handler and passed device from main function

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import os
-import torch
-import huggingface_hub
 
-from loguru import logger
+import huggingface_hub
+import torch
 from dotenv import load_dotenv
+from loguru import logger
 from transformers import HfArgumentParser
 
-from src.train import model_train
-from src.eval import model_eval
 from src.config import ScriptArguments
-
+from src.eval import model_eval
+from src.test import model_test
+from src.train import model_train
 
 if __name__ == "__main__":
     load_dotenv()
     torch.manual_seed(42)
 
+    device: torch.device
+
     if torch.cuda.is_available():
+        device = torch.device("cuda")
         logger.info("GPU with CUDA available.")
     else:
+        device = torch.device("cpu")
         logger.info("Could not find any CUDA supported device.")
 
     __parser = HfArgumentParser(ScriptArguments)
-    script_args: ScriptArguments = __parser.parse_args_into_dataclasses()[0]
+    config: ScriptArguments = __parser.parse_args_into_dataclasses()[0]
 
     huggingface_hub.login(os.getenv("HF_ACCESS_TOKEN"))
 
-    match (script_args.mode):
+    match (config.mode):
         case "train":
-            model_train(script_args)
+            model_train(config, device)
         case "test":
-            raise NotImplementedError("'test' functionality not yet implemented.")
+            model_test(config, device)
         case "eval":
-            model_eval(script_args)
+            model_eval(config, device)
         case _:
             raise NotImplementedError("mode can only be train, text or eval.")
