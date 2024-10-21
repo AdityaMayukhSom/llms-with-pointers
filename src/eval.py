02145453c7b51d3067b593e761a2484f89b22ab1
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-10-21 18:07:20 +0530

    finally, a custom model that works

--- src/eval.py
+++ src/eval.py
@@ -3,6 +3,7 @@ from loguru import logger
 from src.config import ScriptArguments
 from src.model import PointerGeneratorLlama, create_and_prepare_model
 from transformers import LogitsProcessorList
+from transformers import LlamaForCausalLM
 from transformers.generation import GenerateDecoderOnlyOutput
 
 
@@ -11,23 +12,12 @@ def model_eval(config: ScriptArguments):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     prompt = ["What is Google?"]
 
-    # model, peft_config, tokenizer = create_and_prepare_model(config)
-    # inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True).to(device=device)
-    # outputs: GenerateDecoderOnlyOutput = model.generate(
-    #     **inputs,
-    #     max_new_tokens=1,
-    #     num_return_sequences=1,
-    #     output_logits=True,
-    #     output_scores=True,
-    #     output_attentions=True,
-    #     return_dict_in_generate=True,
-    # )
-
-    model = PointerGeneratorLlama(config)
-    inputs = model.tokenizer(prompt, return_tensors="pt", padding=True, truncation=True).to(device=device)
-    outputs: GenerateDecoderOnlyOutput = model(
+    model, peft_config, tokenizer = create_and_prepare_model(config)
+    model = model.to(device=device)
+    inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True).to(device=device)
+    outputs: GenerateDecoderOnlyOutput = model.generate(
         **inputs,
-        max_new_tokens=1,
+        max_new_tokens=200,
         num_return_sequences=1,
         output_logits=True,
         output_scores=True,
@@ -45,5 +35,5 @@ def model_eval(config: ScriptArguments):
     # final_dist = next_token_login + project_attention_on_vocab(attentions)
     # final_tokens = torch.argmax(final_dist)
     # final_words = tokenizer.decode(final_tokens, skip_special_tokens=True)
-    # text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    # logger.info(text)
+    text = tokenizer.decode(outputs.sequences[0], skip_special_tokens=True)
+    logger.info(text)
