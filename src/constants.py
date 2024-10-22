5b10f65643d49584098e723157ffa8c9d628caa1
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-10-22 23:17:14 +0530

    extracted prompt structures to constant file

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,25 @@
 import enum
 
+INSTRUCT_PROMPT_TEMPLATE = """\
+<|begin_of_text|>
+<|start_header_id|>system<|end_header_id|>
+{system_message}
+<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+{user_message}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>"""
+
+SYSTEM_MESSAGE = """\
+You are an expert in reading articles and coming up with brief, yet clear and informative abstracts.
+Only return a generated abstract. Do not provide any explanations or unnecessary words. 
+Do not provide anything which is not necessary or related to the abstract. Just provide the abstract."""
+
+USER_MESSAGE_TEMPLATE = """\
+Summarize this following article under {max_words} words:
+
+{article}"""
+
 
 @enum.unique
 class DataPointKeys(str, enum.Enum):
--- src/dataset.py
+++ src/dataset.py
@@ -5,27 +5,12 @@ from tfrecord.torch.dataset import TFRecordDataset
 from torch.utils.data import IterableDataset
 from torch.utils.data._utils.collate import default_collate
 
-from src.constants import DataPointKeys
-
-INSTRUCT_PROMPT_TEMPLATE = """\
-<|begin_of_text|>
-<|start_header_id|>system<|end_header_id|>
-{system_message}
-<|eot_id|>
-<|start_header_id|>user<|end_header_id|>
-{user_message}
-<|eot_id|>
-<|start_header_id|>assistant<|end_header_id|>"""
-
-SYSTEM_MESSAGE = """\
-You are an expert in reading articles and coming up with brief, yet clear and informative abstracts.
-Only return a generated abstract. Do not provide any explanations or unnecessary words. 
-Do not provide anything which is not necessary or related to the abstract. Just provide the abstract."""
-
-USER_MESSAGE_TEMPLATE = """\
-Summarize this following article under {max_words} words:
-
-{article}"""
+from src.constants import (
+    INSTRUCT_PROMPT_TEMPLATE,
+    SYSTEM_MESSAGE,
+    USER_MESSAGE_TEMPLATE,
+    DataPointKeys,
+)
 
 
 def generate_prompt_from_article(article: str, requested_max_words: int):
