d7a27b603c6094062c330aa978b61199e3f271d5
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-10-24 18:28:20 +0530

    Added MetricsUtils utility class
    - Class focused to generate different static methods to be used
      as decorators for gathering metrics

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,29 @@
 import hashlib
 import os
+import time
 from concurrent.futures import ThreadPoolExecutor
-from typing import List
+from functools import wraps
+from typing import Callable, List, ParamSpec, TypeVar
 
 from loguru import logger
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+
+class MetricsUtils:
+    @staticmethod
+    def execution_time(func: Callable[P, T]):
+        @wraps(func)
+        def execution_time_wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+            start_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            total_time = end_time - start_time
+            print(f"Function {func.__name__} Took {total_time:.4f} seconds")
+            return result
+
+        return execution_time_wrapper
 
 
 class ResultsUtils:
