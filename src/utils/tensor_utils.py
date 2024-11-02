e26926922baf83809bd1b61c9e2f49adbd76700a
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-11-03 00:19:23 +0530

    Added function to calculate NaN

--- src/utils/tensor_utils.py
+++ src/utils/tensor_utils.py
@@ -15,3 +15,10 @@ class TensorUtils:
             print(tensor_name.ljust(24), type(torch_tensor).__name__, torch_tensor.dtype, torch_tensor.shape, sep="\t")
         else:
             print(f"{tensor_name} is not a tensor, rather it is of type `{type(torch_tensor).__name__}`")
+
+    @staticmethod
+    def print_nan_count(torch_tensor: torch.Tensor, tensor_name: str):
+        nan_count = torch.sum(torch.isnan(torch_tensor).int()).item()
+        if TensorUtils.__DEBUG_MODE:
+            print("nan count", tensor_name.ljust(16), nan_count)
+        return nan_count
