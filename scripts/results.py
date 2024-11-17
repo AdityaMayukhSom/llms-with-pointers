d7884126bff8e9eafd70e48ba45c31e852c86c85
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-11-18 04:46:55 +0530

    Reduced percentile display

--- scripts/results.py
+++ scripts/results.py
@@ -7,4 +7,4 @@ pd.set_option("display.max_colwidth", None)
 
 if __name__ == "__main__":
     df = pd.read_csv("./rouge_data/results.csv", index_col=0)
-    print(df.describe())
+    print(df.describe(percentiles=[]))
