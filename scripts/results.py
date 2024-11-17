382b7ba4756200a9007accf8523ca3769ae56352
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-11-17 19:21:47 +0530

    Display generated metrics

---
+++ scripts/results.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 2000)
+pd.set_option("display.max_colwidth", None)
+
+if __name__ == "__main__":
+    df = pd.read_csv("./rouge_data/results.csv", index_col=0)
+    print(df.describe())
